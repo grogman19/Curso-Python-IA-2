74ce32a48fc6b7877e4d9d10413ac17d9009d1fb
Asier Ibáñez <asier.ibanez@gmail.com>
2021-06-01 00:54:30 +0200

    Parte obligatoria terminada

---
+++ Proyecto Final/Parte 2.py
@@ -0,0 +1,113 @@
+# Para la segunda parte del proyecto, crearemos un dashboard interactivo usando streamlit, en el que podremos introducir los datos
+# de edad, sexo, y clase, de un hipotético pasajero del Titanic, y mediante el clasificador entrenado en la primera parte, hacer
+# una predicción sobre si el pasajero hubiera sobrevivido al hundimiento o no.
+
+# Comenzaremos por importar el módulo streamlit, y el resto de módulos necesarios para trabajar con el clasificador
+import streamlit as st
+import pandas as pd
+import pickle
+import time
+from PIL import Image
+
+# Mostramos un título y una breve explicación de lo que hace el dashboard
+st.title('Simulador de supervivencia del Titanic')
+st.write('''Bienvenido al simulador de supervencia al desastre del Titanic. Si desea sentir de primera mano 
+            lo que pudo ser la experiencia del hundimiento del Titanic, introduzca sus datos de Sexo, Edad, y en que
+            clase le gustaría viajar, y gustosamente le indicaremos si sobrevivió al desastre... o no.''')
+
+# Crearemos un form de Streamlit, para recargar la app únicamente cuando le demos al botón de 
+# calcular, y no al interactuar con los otros controles
+form_datos_entrada = st.form(key='datos_entrada')
+# Introducimos un combo box para elegir el Sexo
+sexo = form_datos_entrada.selectbox('Seleccione su sexo:', ('Hombre', 'Mujer'))
+# Introducimos un campo de texto libre para la Edad
+edad = form_datos_entrada.text_input(label='Introduzca su edad:') 
+# Introducimos un combo box para elegir la Clase
+clase = form_datos_entrada.selectbox('Ud. desea viajar en clase:', ('Primera', 'Segunda', 'Tercera'))
+# Introducimos un botón que procederá a realizar el cálculo
+boton_calculo = form_datos_entrada.form_submit_button(label='Calcular')
+
+# Importamos los datos del clasificador para hacer la predicción
+# Comenzamos abriendo el fichero serializado con Pickle
+with open('titanic_model.pkl', 'rb') as f:
+    info = pickle.load(f)
+        
+# Extraemos los datos del diccionario
+knc_titanic = info['modelo']
+sc_titanic = info['scaler']
+feature_names = info['feature_names']
+target_name = info['target_name']
+categorias_sexo = info['categorias']['Sex']
+
+# Creamos un diccionario donde guardaremos los datos recibidos del usuario
+dato = dict()
+
+# Cargamos un par de imagenes que mostraremos en función del resultado
+img_ok = Image.open('OK.jpg')
+img_ko = Image.open('KO.jpg')
+    
+# Esta sección se ejecuta al presionar el botón Calcular    
+if boton_calculo:
+    # Sacamos el dato del sexo
+    if sexo == 'Hombre':
+        dato['Sex'] = categorias_sexo['male']
+    else:
+        dato['Sex'] = categorias_sexo['female']
+    
+    # Sacamos el dato de la clase
+    if clase == 'Primera':
+        dato['Pclass'] = 1
+    elif clase == 'Segunda':
+        dato['Pclass'] = 2
+    else:
+        dato['Pclass'] = 3
+
+    # Sacamos el dato de la edad llevando control de los errores
+    int_exception = False
+    try:
+        dato['Age'] = int(edad)
+    except ValueError:
+        st.error('ERROR: La edad debe de ser un número entero')
+        int_exception = True
+    
+    # Si no ha habido excepción con la conversión de la edad a entero, continuamos
+    if int_exception == False:
+        # Hacemos un poco el tonto con una progress bar y mensajes de status para hacer más simpatico el dashboard y
+        # que parezca que hace algo más sofisticado
+        progress_bar = st.progress(0)
+        status_text = st.empty()
+        status_text.text('Ajustando modelo calibrando la hipótesis nula')
+        for i in range(100):
+            progress_bar.progress(i + 1)
+            if i == 24:
+                status_text.text('Entrenando red neuronal con un billón de hiperparámetros')
+            elif i == 49:
+                status_text.text('Entrenando red GaN para optimizar el resultado')
+            elif i == 74:
+                status_text.text('Precalentando Tensor Cores de la GPU para generar la predicción')
+            elif i == 99:
+                status_text.text('Cálculo de la predicción completado')
+            time.sleep(0.1)
+
+        # Y ahora sí, procedemos a realizar la predicción
+        # Creamos un dataframe de pandas con los datos para la predicción
+        X = pd.DataFrame()
+        for categoria in feature_names:
+            X.loc[0, categoria] = dato[categoria]
+        
+        # Normalizamos los datos de entrada con el scaler
+        X_std = sc_titanic.transform(X)
+
+        # Realizamos la predicción
+        Y = pd.DataFrame()
+        Y[target_name] = knc_titanic.predict(X_std)
+
+        # Mostramos el resultado en pantalla
+        if Y.loc[0, target_name] == 0:
+            st.title('Ooooohhhh, lo sentimos, pero no había suficiente sitio en la tabla y NO HAS SOBREVIVIDO :(')
+            st.image(img_ko)
+        else:
+            st.title('¡¡¡Enhorabuena!!! Había sitio en la tabla para ti, y HAS SOBREVIVIDO :)')
+            st.image(img_ok)
+
+
